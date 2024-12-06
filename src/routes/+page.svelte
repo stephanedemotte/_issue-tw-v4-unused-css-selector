d52a7aa190a6e0b8814244351b18eb1634d4a27f
Stephane Demotte <stephanedemotte@mac-2.local>
2024-12-06 10:10:47 -0500

    big bang

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "$lib/style.css";
+</script>
+
+<h1 class="bg-red-500">Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<style>
+  @import "$lib/style.css" reference;
+  p {
+    @apply bg-green-500;
+  }
+</style>
